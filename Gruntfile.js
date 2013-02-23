e34c404bb0ab9f281afb3027a59f58ca93eaa88e
Casey Thomas <cpthomas@gmail.com>
2013-02-23 17:55:15 -0500

    Fix up grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,22 @@
-grunt.initConfig({
-  compass: {
-    dist: {
-      options: {
-        sassDir: 'sass',
-        cssDir: 'www/css',
-        environment: 'production'
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    compass: {
+      dist: {
+        options: {
+          sassDir: 'sass',
+          cssDir: 'www/css',
+          environment: 'production'
+        }
       }
+    },
+    jshint: {
+      all: ['www/js/app.js']
     }
-  }
-});
+  });
 
-grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
 
-grunt.registerTask('compass', ['compass']);
-grunt.registerTask('default', ['jshint', 'compass']);
+  grunt.registerTask('default', ['jshint', 'compass']);
+};
